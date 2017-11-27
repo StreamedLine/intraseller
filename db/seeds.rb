78d04fe26e5818d3e2ea51a1c5a75000dce0e605
streamedLine <dovidlwtn@gmail.com>
2017-11-26 22:11:13 -0500

    add notes to seed file. writing list of models and associations. ready to start generating models

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ User.create(email: 'dave@dave.com', password: 123456789)
 
 category = Category.create(name: 'photo')
 
-item1 = Item.create(bhsku: 'SODSCHX90VB', mfrsku: 'DSCHX90V/B', category_id: category.id, image: 'https://static.bhphoto.com/images/images250x250/1481216759000_1137175.jpg')
-item2 = Item.create(bhsku: 'NICPW300B', mfrsku: '26523', category_id: category.id, image: 'https://static.bhphoto.com/images/images250x250/1496213450000_1341600.jpg')
+item1 = Item.create(bhsku: 'SODSCHX90VB', mfrsku: 'DSCHX90V/B', image: 'https://static.bhphoto.com/images/images250x250/1481216759000_1137175.jpg')
+item2 = Item.create(bhsku: 'NICPW300B', mfrsku: '26523', image: 'https://static.bhphoto.com/images/images250x250/1496213450000_1341600.jpg')
 
 question = category.questions.create(question: 'Which point and shoot cameras have built in GPS?')
 
@@ -12,3 +12,39 @@ category.questions.last.answers.last.items << [item1, item2]
 
 category.save
 
+
+#= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =#
+
+        Category 
+---------------------
+
+categories have many items, through: categoryItems
+
+categories have many questions, through: categoryQuestions
+
+        Item
+---------------------
+
+items have many categories, through: categoryItems
+
+items have many links, through: itemLink
+
+        Question
+---------------------
+
+questions have many answers
+
+        Answer
+---------------------
+
+answer belongs to one question
+
+    CategoryItem
+---------------------
+    CategoryQuestion
+---------------------
+      ItemLink
+---------------------
+
+
+#= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =#
