341e62066f76bbc28d37822522f26570d57422f0
streamedLine <dovidlwtn@gmail.com>
2017-12-14 12:32:31 -0500

    allow the jointable categoryItem to have user-updatable attribute note about unique relationship

--- app/controllers/category_items_controller.rb
+++ app/controllers/category_items_controller.rb
@@ -3,8 +3,24 @@ class CategoryItemsController < ItemsController
     @category = category
   end
 
+  def update
+    if params[:category_item].present?
+      @category = category
+      @category_item = CategoryItem.find(params[:id])
+      @category_item.update(category_items_params)
+      redirect_to category_path(@category)
+    else
+      super
+    end
+  end
+
   private
   
+  def category_items_params
+    params.require(:category_item).permit(:note)
+  end
+
+  #All of the below methods for when item is created within context of category
   def new_from_params
     @category = category
     @category.find_or_build_item_from_params(item_params)
