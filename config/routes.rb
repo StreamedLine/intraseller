9bafdfc7851273f3b43bf878e81676ebb90c8c4e
StreamedLine <Dovidlwtn@gmail.com>
2017-12-14 09:57:54 -0500

    create routes for comarisons table. routes are nested within items

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :items, only: [:index, :show, :new, :create, :edit, :update] do 
   	resources :questions, only: [:create, :update], :controller => "item_questions"
+  	resources :comparisons, only: [:show, :new, :create]
   end
 
 end
