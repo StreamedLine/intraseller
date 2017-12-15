52bea5314257300eeb7bfed4b8b4dee72ef378cd
streamedLine <dovidlwtn@gmail.com>
2017-12-15 14:37:19 -0500

    flesh out comparisonController

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -1,28 +1,43 @@
 class ComparisonsController < ApplicationController
 	def index 
-		@comparisons = Comparison.all
+		@comparisons = item.comparisons.all
 	end
 
 	def show
-		@comparison = Comparison.find(params[:id])
+		@comparison = item.comparison.find(params[:id])
 	end
 
 	def create
-		@comparison = Comparison.new
+		@comparison = Comparison.new(comparison_params)
 
 		if @comparison.save
+			flash[:notice] = "Comparison saved"
 			redirect_to comparison_path(@comparison)
 		else
-			binding.pry
+			flash[:error] = "Comparison not saved"
+			redirect_to item_path(item)
 		end
 	end
 
 	def update
+		@comparison = Comparison.find(params[:id])
+
+  	if @comparison.update(comparison_params)
+      flash[:notice] = "Success comparison updated"
+  		redirect_to comparison_path(@comparison)
+  	else
+      flash[:error] = "#{@comparison.errors.full_messages.first}"
+  		redirect_to item_path(item)
+  	end
 	end
 
 	private
 
 	def comparison_params
 		params.require(:comparison).permit(:item_id, :compared_id, :bullets_attributes => [:nugget])
+	end
+
+	def item
+		item = params[:item_id]
 	end
 end
